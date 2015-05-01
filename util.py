3cee22d794f8635a4f4f80c1b629aa922aebb8b5
Andrej Maris <4ndrejm@gmail.com>
2015-05-01 12:14:23 +0100

    Generates random perturbations of initial shape

--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 import menpo
+import random
 from menpo import io as mio
 from menpo.shape import PointCloud
 from menpo.transform import AlignmentSimilarity
@@ -39,9 +40,24 @@ def fit_shape_to_box(normal_shape, box):
     #return PointCloud([ [p[0]*w + box[0][0], p[1]*h+box[0][1]] for p in normal_shape.points])
 
 
+# TODO: Use OpenCV for this!
 def get_bounding_box(img):
     if not img.has_landmarks:
         return [[0,0], [0,0]]
     l = zip(*list(img.landmarks['PTS'].lms.points))
     box = [ [min(l[0]), min(l[1])], [max(l[0]), max(l[1])] ]
+
+    # image.landmarks['PTS'].lms.bounds()!!
+
     return box
+
+def perturb(bounding_box):
+    width = bounding_box[1][0] - bounding_box[0][0]
+    height = bounding_box[1][1] - bounding_box[0][1]
+
+    for i in xrange(2):
+        dx = random.uniform(-0.1*width, 0.1*width)
+        dy = random.uniform(-0.1*height, 0.1*height)
+
+        bounding_box[i] += [dx, dy]
+    return bounding_box
