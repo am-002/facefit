ad197d9263dd32e2a810da94189c084b6229fe05
Andrej Maris <4ndrejm@gmail.com>
2015-05-05 22:00:11 +0100

    Increases performance by using numpy arrays

--- primary_regressor.py
+++ primary_regressor.py
@@ -22,63 +22,47 @@ class PrimaryRegressor:
         self.feature_extractor = PixelExtractor(P, nLandmarks, mean_shape, self.kappa)
         self.primitive_regressors = [PrimitiveRegressor(P, F, nFerns, nLandmarks) for i in range(nFerns)]
 
-    def extract_features(self, img, shape):
-        return self.feature_extractor.extract_pixels(img, shape, transform_to_mean_shape(shape, self.mean_shape).pseudoinverse())
+    def extract_features(self, img, shape, mean_to_shape, dest):
+        return self.feature_extractor.extract_pixels(img, shape, mean_to_shape, dest)
 
     def train(self, pixel_vectors, targets):
         n_samples = len(pixel_vectors)
-        ##print pixel_vectors
-        pixel = zip(*pixel_vectors)
 
-        #print 'pixels: ', pixel
-
-        pixels_sum = map(sum, pixel)
-        cov_pp = np.cov(pixel)
+        # Precompute a bunch of useful numbers.
+        pixel_values = np.matrix.transpose(pixel_vectors)
+        cov_pp = np.cov(pixel_values)
 
         i = 0
         for r in self.primitive_regressors:
-            #print 'Training regressor ', i
+            print 'Training primitive regressor', i
             i += 1
-            r.train(pixel_vectors, targets, cov_pp, pixels_sum, pixel)
+            r.train(pixel_vectors, targets, cov_pp, pixel_values)
             for j in xrange(n_samples):
-                targets[j].points -= r.apply(pixel_vectors[j]).points
-                #print targets[j].points
+                targets[j] -= r.apply(pixel_vectors[j])
 
     def apply(self, shape, shape_indexed_features):
-        res = PointCloud([[0.0, 0.0] for i in range(self.nLandmarks)])
-        #mean_to_shape = AlignmentSimilarity(self.mean_shape, shape)
-
+        res = np.zeros(shape=(self.nLandmarks, 2), dtype=float)
         for r in self.primitive_regressors:
-            #offset = mean_to_shape.apply(r.apply(shape_indexed_features))
-            offset = r.apply(shape_indexed_features)
-            res.points += offset.points
+            res += r.apply(shape_indexed_features)
         return res
 
 
 class PixelExtractor:
     def __init__(self, n_pixels, n_landmarks, mean_shape, kappa):
         self.n_pixels = n_pixels
-        self.pixel_coords = []
+        self.pixel_coords = np.random.uniform(low=-kappa, high=kappa, size=2*n_pixels).reshape(n_pixels, 2)
+        self.landmarks = np.random.randint(low=0, high=n_landmarks, size=n_pixels)
         self.mean_shape = mean_shape
 
-        for i in xrange(n_pixels):
-            lmark = random.randint(0, n_landmarks-1)
-            #TODO: normalize this.
-            dx = random.uniform(-kappa, kappa)
-            dy = random.uniform(-kappa, kappa)
-            self.pixel_coords.append((lmark, [dx, dy]))
-
-    def extract_pixels(self, image, shape, mean_to_shape):
-        ret = []
-        for (lmark, offset) in self.pixel_coords:
-            offset = mean_to_shape.apply(offset)
-            x = int(shape.points[lmark][0] + offset[0])
-            y = int(shape.points[lmark][1] + offset[1])
+    def extract_pixels(self, image, shape, mean_to_shape, dest):
+        for i in xrange(self.n_pixels):
+            lmark = self.landmarks[i]
+            offset = mean_to_shape.apply(self.pixel_coords[i])
+            x = int(shape[lmark][0] + offset[0])
+            y = int(shape[lmark][1] + offset[1])
 
             if x < 0 or y < 0 or x >= image.shape[0] or y >= image.shape[1]:
-                ret.append(0)
+                dest[i] = 0
                 continue
             # Extract pixel at (x, y).
-            ret.append(image.pixels[x][y][0])
-
-        return ret
+            dest[i] = image.pixels[x][y][0]
--- primitive_regressor.py
+++ primitive_regressor.py
@@ -1,12 +1,9 @@
-import random
-import menpo
-from menpo.shape import PointCloud
 import numpy as np
+import weave
 
 INF = 102345678
-BETA = 1000
-# TODO !!!
-#BETA = 0.0
+# BETA = 1000
+BETA = 0
 
 class PrimitiveRegressor:
     def __init__(self, n_features, n_fern_features, n_ferns, n_landmarks):
@@ -17,17 +14,20 @@ class PrimitiveRegressor:
         self.fern = Fern(n_fern_features, n_landmarks)
         self.fern_feature_selector = FeatureSelector(n_features, n_fern_features)
 
-    def train(self, feature_vectors, targets, cov_pp, pixels_sum, pixels):
-        self.fern_feature_selector.train(feature_vectors, targets, cov_pp, pixels_sum, pixels)
-        fern_feature_vectors = []
-        for feature_vector in feature_vectors:
-            fern_feature_vectors.append(self.fern_feature_selector.extract_features(feature_vector))
+    def train(self, feature_vectors, targets, cov_pp, pixels):
+        self.fern_feature_selector.train(feature_vectors, targets, cov_pp, pixels)
+
+        fern_feature_vectors = np.ndarray(shape=(len(feature_vectors), self.nFernFeatures), dtype=float)
+        for i in xrange(len(feature_vectors)):
+             self.fern_feature_selector.extract_features(feature_vectors[i], fern_feature_vectors[i])
+
         self.fern.train(fern_feature_vectors, targets)
 
-    def apply(self, shapeIndexedFeatures):
-        fern_features = self.fern_feature_selector.extract_features(shapeIndexedFeatures)
+    def apply(self, shape_indexed_features):
+        fern_features = np.zeros(shape=(self.nFernFeatures,))
+        self.fern_feature_selector.extract_features(shape_indexed_features, fern_features)
 
-        return self.fern.test(fern_features)
+        return self.fern.apply(fern_features)
 
 
 class Fern:
@@ -35,157 +35,121 @@ class Fern:
     Implementation of a random Fern.
 
     """
-    class Bin:
-        def __init__(self, n_landmarks):
-            self.size = 0.0
-            self.delta_sum = menpo.shape.PointCloud([[0.0, 0.0]] * n_landmarks)
-
-        def add(self, delta):
-            self.size += 1.0
-            self.delta_sum.points += delta.points
-
-        def get_delta(self):
-            if self.size == 0.0:
-                return self.delta_sum
-            return PointCloud(self.delta_sum.points / (self.size + BETA))
-
 
     def __init__(self, n_features, n_landmarks):
         self.n_features = n_features
-        self.thresholds = [0] * self.n_features
-        self.bins = [Fern.Bin(n_landmarks) for _ in range(2 ** n_features)]
+        self.thresholds = np.zeros(n_features, dtype=float)
+        self.bins = np.zeros(shape=(2**n_features, 68, 2), dtype=float)
+        self.bin_sz = np.zeros(shape=(2**n_features), dtype=float)
 
     def get_bin(self, feature_vector):
-        res = 0
-        for i in range(self.n_features):
-            if feature_vector[i] <= self.thresholds[i]:
-                res |= 1 << i
-        return res
+        thresholds = self.thresholds
+        n_features = self.n_features
+        code = """
+            int res = 0;
+            for (int i = 0; i < n_features; ++i) {
+                if (feature_vector[i] <= thresholds[i])
+                    res |= 1 << i;
+            }
+            return_val = res;
+        """
+        return weave.inline(code, ['thresholds', 'n_features', 'feature_vector'])
 
     def train(self, feature_vectors, targets):
         n_samples = len(feature_vectors)
 
-        # Generate thresholds for each features. We first get the ranges
+        # Generate thresholds for each feature. We first get the ranges
         # of all features in the training set.
         ranges = [(INF, -INF)] * self.n_features
+        #ranges = np.ndarray(shape=(self.n_features, 2))
 
         for feature_vector in feature_vectors:
-            for i, feature in enumerate(feature_vector):
-                ranges[i] = (min(ranges[i][0], feature), max(ranges[i][1], feature))
+           for i, feature in enumerate(feature_vector):
+               ranges[i] = (min(ranges[i][0], feature), max(ranges[i][1], feature))
 
-        # Generate a random threshold for each feature.
-        self.thresholds = [random.uniform(rng[0], rng[1]) for rng in ranges]
+        for i, rng in enumerate(ranges):
+            self.thresholds[i] = np.random.uniform(low=rng[0], high=rng[1])
 
         for i in xrange(n_samples):
             bin_id = self.get_bin(feature_vectors[i])
-            self.bins[bin_id].add(targets[i])
+            self.bins[bin_id] += targets[i]
+            self.bin_sz[bin_id] += 1
 
-    def test(self, feature_vector):
+    def apply(self, feature_vector):
         bin_id = self.get_bin(feature_vector)
-        return self.bins[bin_id].get_delta()
+
+        if self.bin_sz[bin_id] == 0.0:
+            return np.zeros(shape=(68, 2), dtype=float)
+
+        return self.bins[bin_id] / (self.bin_sz[bin_id] + BETA)
 
 
 class FeatureSelector:
-    def __init__(self, nPixels, F):
-        self.nPixels = nPixels
-        self.F = F
-        self.features = [(0,0)]*F
-        random.seed(None)
+    def __init__(self, n_pixels, n_fern_features):
+        self.nPixels = n_pixels
+        self.n_fern_features = n_fern_features
+        self.features = np.zeros(shape=(n_fern_features, 2))
 
-    def average(self, x):
-        return float(sum(x)) / len(x)
+    def train(self, pixel_vectors, targets, cov_pp, pixel_values):
+        l = np.zeros(len(targets), dtype=float)
 
-    # TODO: Random projection from unit gaussian?
-    def getRandomDirection(self):
-        return np.array([random.uniform(-1, 1) for i in range(2*68)])
-
-    def magnitude(self, x):
-        return np.sqrt(np.dot(x, x))
-
-    # Project a onto b and return length of the projection.
-    def project(self, a, b):
-        return np.dot(a, b) / (self.magnitude(a) * self.magnitude(b));
-
-    def cov(self, x, y):
-        mean_x = self.average(x)
-        mean_y = self.average(y)
-        res = 0.0
-        for i in range(len(x)):
-            res += (x[i]-mean_x) * (y[i]-mean_y)
-        return res / float(len(x))
-
-
-    def train(self, features, targets, cov_pp, pixel_sum, pixels):
-
-
-        for f in xrange(self.F):
-            #print 'COV_PP: ', cov_pp
+        for f in xrange(self.n_fern_features):
             # Project the offset in random direction and measure the scalar length
-            # of the projection. Find which feature has highest imapct on the length.
+            # of the projection. Find which feature has highest impact on the length.
 
-            d = self.getRandomDirection()
+            # Get random direction.
+            dir = np.random.randn(136)
 
-            l = []
+            # Normalize it.
+            dir /= np.linalg.norm(dir)
 
-            for target in targets:
-                l.append(self.project(target.as_vector(), d))
-            #print 'l is ', l
+            # Calculate projections of each target onto the random direction.
+            l = targets.reshape((len(targets), 136)).dot(dir)
 
-            l_sum = np.sum(l)
-            var_l = self.cov(l, l)
-
-            # set l to l[i]-l_mean as a speed up!
-
-            cov_l_p = [0]*self.nPixels
-            n_samples = float(len(features))
+            # Calculate variance of projections.
+            var_l = np.var(l)
             n_pixels = self.nPixels
-            # for i in xrange(len(features)):
-            #     li = l[i]
-            #     for pixel in xrange(n_pixels):
-            #         cov_l_p[pixel] = features[i][pixel]*li
-            #
-            # for pixel in xrange(n_pixels):
-            #     cov_l_p[pixel] = (cov_l_p[pixel] + pixel_sum[pixel]*l_sum*(n_samples-2/n_samples))/n_samples
-            #
-            # for pixel in xrange(n_pixels):
-            #     cov_l_p[pixel] /= float(n_pixels-1)
 
+            # Calculate covariances between projections and each pixel.
+            cov_l_p = np.zeros(shape=(self.nPixels), dtype=float)
             for i in xrange(self.nPixels):
-                cov_l_p[i] = self.cov(l, pixels[i])
+                cov_l_p[i] = np.cov(m=l, y=pixel_values[i])[0][1]
 
-            #print 'var(l) = ', var_l
-            #print 'cov_l_p = ', cov_l_p
+            max_corr_code = """
+                float maxcorr = -12345;
+                int res = -1;
+                double varl = var_l;
 
-            #    cov_l_p[pixel] = self.cov(l, p)
+                for (int i = 0; i < n_pixels-1; ++i) {
+                    double var_pii = cov_pp[i*n_pixels+i];
+                    int row = i*n_pixels;
+                    for (int j = i+1; j < n_pixels; ++j) {
+                        double denom = varl*(var_pii + cov_pp[j*n_pixels]-2*cov_pp[row+j]);
+                        if (denom <= 0) {
+                            continue;
+                        }
+                        float corr = (cov_l_p[i] - cov_l_p[j]) / sqrt(denom);
+                        if (corr < 0) {
+                            corr = -corr;
+                        }
+                        if (corr > maxcorr) {
+                            maxcorr = corr;
+                            res = i*n_pixels + j;
+                        }
+                    }
+                }
+                return_val = res;
+            """
 
+            res = weave.inline(max_corr_code, ['cov_pp', 'n_pixels', 'cov_l_p', 'var_l'])
 
-            var_p = [cov_pp[i][i] for i in xrange(self.nPixels)]
-
-            maxcorr = -12345
-            res = (0, 0)
-            for i in xrange(n_pixels):
-                cov_ppii = var_p[i]
-                cov_l_pi = cov_l_p[i]
-                for j in xrange(i+1, n_pixels):
-                    # We want to get corr(l, pixel_i - pixel_j).
-                    denom = var_l*(cov_ppii + var_p[j]-2*cov_pp[i][j])
-                    if (denom <= 0):
-                        # TODO: What to do in this case?
-                        continue
-                    # TODO: abs here?
-                    corr = (cov_l_pi - cov_l_p[j]) / np.sqrt(denom)
-                    #print corr
-                    if (corr < 0):
-                        corr = -corr
-
-                    if corr > maxcorr:
-                        maxcorr = corr
-                        res = (i, j)
-            self.features[f] = res
+            # Extract pixel pair from res and add it to self.features.
+            self.features[f] = (res/n_pixels, res%n_pixels)
 
             # TODO: How to make sure that this feature will not be chosen again? (If
             # it is correlated with another random projection length).
-            # Here the features array is populated
 
-    def extract_features(self, pixels):
-        return [pixels[p[0]] - pixels[p[1]] for p in self.features]
+    def extract_features(self, pixels, dest):
+        for i in xrange(self.n_fern_features):
+            dest[i] = pixels[self.features[i][0]] - pixels[self.features[i][1]]
+        #return [pixels[p[0]] - pixels[p[1]] for p in self.features]
