d37a2d4d1d16326a86b1fd2f8b19bf41e1da3f58
Andrej Maris <4ndrejm@gmail.com>
2015-05-07 22:22:16 +0100

    Further optimizations and refactoring

--- explicit_shape_regressor.py
+++ explicit_shape_regressor.py
@@ -1,14 +1,7 @@
-import sys
-
-from menpo.shape import PointCloud
-import menpo.io as mio
-
 from primary_regressor import PrimaryRegressor
 import util
 import numpy as np
 from util import *
-import copy
-
 
 class ExplicitShapeRegressor:
     def __init__(self, n_landmarks, n_regressors=10, n_pixels=400, n_fern_features=5, n_ferns=500, n_perturbations=20):
@@ -16,73 +9,75 @@ class ExplicitShapeRegressor:
         self.n_regressors = n_regressors
         self.n_pixels = n_pixels
         self.n_ferns = n_ferns
+        self.n_fern_features = n_fern_features
         self.n_perturbations = n_perturbations
-
-        # Calculate mean shape from a subset of training data.
-        self.mean_shape = getNormalisedMeanShape('../helen/subset_cropped/')
-
         # TODO: In the paper, the kappa was set to 0.3*distance-between-eye-pupils-in-mean-shape.
         self.kappa = 0.3
 
-        self.regressors = [PrimaryRegressor(n_pixels, n_fern_features, n_ferns, n_landmarks, self.mean_shape, self.kappa)
-                                for i in range(n_regressors)]
-
     def train(self, img_glob):
-        shapes = []
-
+        # Read the training set into memory.
         images = []
         for img_orig in mio.import_images(img_glob):
-                if not img_orig.has_landmarks:
-                    continue
-                # Convert to greyscale
-                images.append(img_orig.as_greyscale())
-        n_samples = len(images)
-        targets = np.ndarray((n_samples*self.n_perturbations, 136), float)
+            if not img_orig.has_landmarks:
+                continue
+            # Convert to greyscale and crop to landmarks.
+            images.append(img_orig.as_greyscale().crop_to_landmarks_proportion_inplace(0.5))
 
-        # TODO: Read everything into memory.
-        # 1. scale down OR
-        # 2. crop to face
-        # 3. calc mean out of trainset
-        # 4. maybe convert from 64bit to 32bit float?
+        # Calculate normalized mean shape centered at the origin.
+        target_shapes = [img.landmarks['PTS'].lms for img in images]
+        self.mean_shape = menpo.shape.mean_pointcloud(target_shapes)
+        self.mean_shape.points = 2*(self.mean_shape.points - self.mean_shape.centre()) / self.mean_shape.range()
+
+        n_images = len(images)
+        n_landmarks = images[0].landmarks['PTS'].n_landmarks
+        n_samples = n_images*self.n_perturbations
+
+        self.regressors = [PrimaryRegressor(self.n_pixels, self.n_fern_features, self.n_ferns, n_landmarks, self.mean_shape, self.kappa)
+                                for i in range(self.n_regressors)]
+
+        targets = np.ndarray((n_samples, 2*n_landmarks))
+        # shapes = np.ndarray((n_samples, n_landmarks, 2))
+        pixels = np.ndarray((n_samples, self.n_pixels))
+
+        # Generate initial shapes with perturbations.
+        shapes = []
+        for i in xrange(n_samples):
+            bounding_box = get_bounding_box(images[i/self.n_perturbations])
+            shapes.append(fit_shape_to_box(self.mean_shape, bounding_box))
+            for j in xrange(self.n_perturbations-1):
+                shapes.append(fit_shape_to_box(self.mean_shape, perturb(bounding_box)))
+
+        # Perform regression in stages.
         for regressor_i, regressor in enumerate(self.regressors):
-            pixels = []
-
-            img_i = 0
-
-            for img in images:
-                if regressor_i == 0:
-                    bounding_box = get_bounding_box(img)
-                    shapes.append(fit_shape_to_box(self.mean_shape, bounding_box))
-                    for j in xrange(self.n_perturbations-1):
-                        shapes.append(fit_shape_to_box(self.mean_shape, perturb(bounding_box)))
-
+            for i, img in enumerate(images):
                 for j in xrange(self.n_perturbations):
-                    index = img_i*self.n_perturbations + j
+                    index = i*self.n_perturbations + j
 
                     delta = PointCloud(img.landmarks['PTS'].lms.points - shapes[index].points)
-                    normalized_target = util.transform_to_mean_shape(shapes[index], self.mean_shape).apply(delta)
+                    shape_to_mean = util.transform_to_mean_shape(shapes[index], self.mean_shape)
+                    normalized_target = shape_to_mean.apply(delta)
 
-                    # targets.append(normalized_target)
                     targets[index] = normalized_target.as_vector()
-                    pixels.append(regressor.extract_features(img, shapes[index]))
+                    mean_to_shape = shape_to_mean.pseudoinverse()
+                    pixels[index] = regressor.extract_features(img, shapes[index], mean_to_shape)
 
-                img_i += 1
+            regressor.train(pixels, targets)
 
-
-            regressor.train(pixels, np.array(targets))
-
-            for i in xrange(len(shapes)):
-                normalized_offset = regressor.apply(shapes[i], pixels[i])
+            for i in xrange(n_samples):
+                normalized_offset = regressor.apply(pixels[i])
                 offset = util.transform_to_mean_shape(shapes[i], self.mean_shape).pseudoinverse().apply(normalized_offset).points
                 shapes[i].points += offset
 
     def fit(self, image, initial_shape):
         image = image.as_greyscale()
-        shape = fit_shape_to_box(initial_shape, get_bounding_box(image))
+        shape = fit_shape_to_box(self.mean_shape, get_bounding_box(image))
 
         for r in self.regressors:
-            pixels = r.extract_features(image, shape)
-            shape.points += util.transform_to_mean_shape(shape, self.mean_shape).pseudoinverse().apply(r.apply(shape, pixels)).points
-            #shape.points += r.apply(shape, pixels).points
+            mean_to_shape = util.transform_to_mean_shape(shape, self.mean_shape).pseudoinverse()
+            pixels = r.extract_features(image, shape, mean_to_shape)
+            normalized_target = r.apply(pixels)
+            offset =  mean_to_shape.apply(normalized_target).points
+            shape.points += offset
+            # shape.pomean_to_shapenverse().apply((shape, pixels)).points
 
         return shape
--- primary_regressor.py
+++ primary_regressor.py
@@ -3,80 +3,57 @@ from menpo.shape import PointCloud
 import numpy as np
 from primitive_regressor import PrimitiveRegressor
 from util import transform_to_mean_shape
+import util
 
 
 class PrimaryRegressor:
-    # landmarkFeatureSelector
-    # primitive_regressors
 
-    def __init__(self, P, F, nFerns, nLandmarks, mean_shape, kappa):
-        self.nFerns = nFerns
-        self.nFernFeatures = F
-        self.nFeatures = P
+    def __init__(self, n_pixels, n_fern_features, n_ferns, n_landmarks, mean_shape, kappa):
+        self.n_ferns = n_ferns
+        self.n_fern_features = n_fern_features
+        self.n_features = n_pixels
         self.mean_shape = mean_shape
         self.kappa = kappa
-        # TODO
-        self.nPixels = P
-        self.nLandmarks = nLandmarks
-        # TODO: Pass argumetns for FeatureExtractorConstructor in a dictionary
-        self.feature_extractor = PixelExtractor(P, nLandmarks, mean_shape, self.kappa)
-        self.primitive_regressors = [PrimitiveRegressor(P, F, nFerns, nLandmarks) for i in range(nFerns)]
+        self.n_pixels = n_pixels
+        self.n_landmarks = n_landmarks
+        self.feature_extractor = PixelExtractor(n_pixels, n_landmarks, self.kappa)
+        self.primitive_regressors = [PrimitiveRegressor(n_pixels, n_fern_features, n_ferns, n_landmarks) for i in range(n_ferns)]
 
-    def extract_features(self, img, shape):
-        return self.feature_extractor.extract_pixels(img, shape, transform_to_mean_shape(shape, self.mean_shape).pseudoinverse())
+    def extract_features(self, img, shape, mean_to_shape):
+        return self.feature_extractor.extract_pixels(img, shape, mean_to_shape)
 
     def train(self, pixel_vectors, targets):
         n_samples = len(pixel_vectors)
-        pixel = np.array(zip(*pixel_vectors))
+        pixel_vals = np.transpose(pixel_vectors)
 
-        #pixels_sum = map(sum, pixel)
-        pixels_sum = []
-        cov_pp = np.cov(pixel)
+        pixel_averages = np.average(pixel_vals, axis=1)
+        cov_pp = np.cov(pixel_vals)
+        pixel_var_sum = np.diag(cov_pp)[:, None] + np.diag(cov_pp)
 
         i = 0
         for r in self.primitive_regressors:
             print 'Training regressor ', i
             i += 1
-            r.train(pixel_vectors, targets, cov_pp, pixels_sum, pixel)
+            r.train(pixel_vectors, targets, cov_pp, pixel_vals, pixel_averages, pixel_var_sum)
             for j in xrange(n_samples):
-                targets[j] -= r.apply(pixel_vectors[j]).as_vector()
-                #print targets[j].points
+                offset = r.apply(pixel_vectors[j]).as_vector()
+                targets[j] -= offset
 
-    def apply(self, shape, shape_indexed_features):
-        res = PointCloud([[0.0, 0.0] for i in range(self.nLandmarks)])
-        #mean_to_shape = AlignmentSimilarity(self.mean_shape, shape)
+    def apply(self, shape_indexed_features):
+        res = PointCloud(np.zeros((self.n_landmarks, 2)), copy=False)
 
         for r in self.primitive_regressors:
-            #offset = mean_to_shape.apply(r.apply(shape_indexed_features))
             offset = r.apply(shape_indexed_features)
             res.points += offset.points
         return res
 
 
 class PixelExtractor:
-    def __init__(self, n_pixels, n_landmarks, mean_shape, kappa):
-        self.n_pixels = n_pixels
-        self.pixel_coords = []
-        self.mean_shape = mean_shape
-
-        for i in xrange(n_pixels):
-            lmark = random.randint(0, n_landmarks-1)
-            #TODO: normalize this.
-            dx = random.uniform(-kappa, kappa)
-            dy = random.uniform(-kappa, kappa)
-            self.pixel_coords.append((lmark, [dx, dy]))
+    def __init__(self, n_pixels, n_landmarks, kappa):
+        self.lmark = np.random.randint(low=0, high=n_landmarks, size=n_pixels)
+        self.pixel_coords = np.random.uniform(low=-kappa, high=kappa, size=n_pixels*2).reshape(n_pixels, 2)
 
     def extract_pixels(self, image, shape, mean_to_shape):
-        ret = []
-        for (lmark, offset) in self.pixel_coords:
-            offset = mean_to_shape.apply(offset)
-            x = int(shape.points[lmark][0] + offset[0])
-            y = int(shape.points[lmark][1] + offset[1])
-
-            if x < 0 or y < 0 or x >= image.shape[0] or y >= image.shape[1]:
-                ret.append(0)
-                continue
-            # Extract pixel at (x, y).
-            ret.append(image.pixels[x][y][0])
-
-        return ret
+        offsets = mean_to_shape.apply(self.pixel_coords)
+        ret = shape.points[self.lmark] + offsets
+        return util.sample_image(image, ret)
