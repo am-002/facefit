99f849b10c364bb3cd3013fec6597b3cbe36e45c
Andrej Maris <4ndrejm@gmail.com>
2015-05-11 21:51:04 +0100

    Modifies the file structure for easier unit testing

--- esr.py
+++ esr.py
@@ -1,17 +1,26 @@
-from level import Level
-import util
 import hickle
+import numpy as np
+import menpo.io as mio
+import menpo
+from menpo.shape import PointCloud
 from util import *
+from fern_cascade import FernCascadeBuilder
 
-class ExplicitShapeRegressor:
-    def __init__(self, n_landmarks, n_regressors=10, n_pixels=400, n_fern_features=5,
+def get_gt_shape(image):
+    return image.landmarks['PTS'].lms
+
+def get_bounding_box(image):
+    return np.array(get_gt_shape(image).bounds())
+
+class ESRBuilder:
+    def __init__(self, n_landmarks=68, n_stages=10, n_pixels=400, n_fern_features=5,
                  n_ferns=500, n_perturbations=20, kappa=0.3, beta=1000):
-        self.n_regressors = n_regressors
+        self.n_landmarks = n_landmarks
+        self.n_stages = n_stages
         self.n_pixels = n_pixels
         self.n_ferns = n_ferns
         self.n_fern_features = n_fern_features
         self.n_perturbations = n_perturbations
-        # TODO: In the paper, the kappa was set to 0.3*distance-between-eye-pupils-in-mean-shape.
         self.kappa = kappa
         self.beta = beta
 
@@ -25,64 +34,70 @@ class ExplicitShapeRegressor:
             images.append(img_orig.as_greyscale(mode='average').crop_to_landmarks_proportion_inplace(0.5))
         return images
 
+    # Fit a shape into a box. The shape has to be normalised and centered around
+    # the origin (inside [-1, -1], [1, 1]).
+    @staticmethod
+    def fit_shape_to_box(normal_shape, box):
+        box = PointCloud(box, copy=False)
+        return PointCloud(normal_shape.points*box.range()/2 + box.centre())
+
     def from_file(self, file):
         return hickle.load(file, safe=False)
 
-    def train(self, images):
-        # Calculate normalized mean shape centered at the origin.
-        target_shapes = [img.landmarks['PTS'].lms for img in images]
-        self.mean_shape = menpo.shape.mean_pointcloud(target_shapes)
-        self.mean_shape.points = 2*(self.mean_shape.points - self.mean_shape.centre()) / self.mean_shape.range()
+    def centered_mean_shape(self, target_shapes):
+        mean_shape = menpo.shape.mean_pointcloud(target_shapes)
+        return PointCloud(2 * (mean_shape.points - mean_shape.centre()) / mean_shape.range())
 
+    def perturb_boxes(self, boxes, n_perturbations):
+        perturbed_positions = np.random.normal(loc=1, scale=0.1, size=(len(boxes)*n_perturbations, 2, 2))
+        return boxes.repeat(n_perturbations, axis=0) * perturbed_positions
 
-        self.n_landmarks = images[0].landmarks['PTS'].n_landmarks
+    def get_gt_shapes(self, images):
+        return [img.landmarks['PTS'].lms for img in images]
 
-        n_images = len(images)
-        n_samples = n_images*self.n_perturbations
+    def get_bounding_boxes(self, images):
+        return np.array([shape.bounds() for shape in self.get_gt_shapes(images)])
 
-        self.regressors = [Level(self.n_pixels, self.n_fern_features, self.n_ferns, self.n_landmarks, self.mean_shape, self.kappa, self.beta)
-                                for i in range(self.n_regressors)]
-
-        targets = np.ndarray((n_samples, 2*self.n_landmarks))
-        pixels = np.ndarray((n_samples, self.n_pixels))
+    def build(self, images):
+        images = np.array(images)
+        self.mean_shape = self.centered_mean_shape([img.landmarks['PTS'].lms for img in images])
 
         # Generate initial shapes with perturbations.
-        shapes = []
-        for i in xrange(n_images):
-            bounding_box = get_bounding_box(images[i])
-            shapes.append(fit_shape_to_box(self.mean_shape, bounding_box))
-            for j in xrange(self.n_perturbations-1):
-                shapes.append(fit_shape_to_box(self.mean_shape, perturb(bounding_box)))
+        shapes = [self.fit_shape_to_box(self.mean_shape, box) for box in
+                        self.perturb_boxes(self.get_bounding_boxes(images), self.n_perturbations)]
 
-        # Perform regression in stages.
-        for regressor_i, regressor in enumerate(self.regressors):
-            for i, img in enumerate(images):
-                for j in xrange(self.n_perturbations):
-                    index = i*self.n_perturbations + j
+        # Repeat each image n_perturbations times. Only shallow-duplicates references.
+        images = images.repeat(self.n_perturbations)
 
-                    delta = PointCloud(img.landmarks['PTS'].lms.points - shapes[index].points)
-                    shape_to_mean = util.transform_to_mean_shape(shapes[index], self.mean_shape)
-                    normalized_target = shape_to_mean.apply(delta)
+        # Extract ground truth shapes from annotated images.
+        gt_shapes = self.get_gt_shapes(images)
 
-                    targets[index] = normalized_target.as_vector()
-                    mean_to_shape = shape_to_mean.pseudoinverse()
-                    pixels[index] = regressor.extract_features(img, shapes[index], mean_to_shape)
+        fern_cascades = []
+        for i in xrange(self.n_stages):
+            fern_cascade_builder = FernCascadeBuilder(self.n_pixels, self.n_fern_features, self.n_ferns,
+                                                      self.n_landmarks, self.mean_shape, self.kappa, self.beta)
+            fern_cascade = fern_cascade_builder.build(images, shapes, gt_shapes)
+            # Update current estimates of shapes.
+            shapes = [fern_cascade.apply(image, shape, transform_to_mean_shape(shape, self.mean_shape))
+                      for image, shape in zip(images, shapes)]
+            fern_cascades.append(fern_cascade)
 
-            regressor.train(pixels, targets)
+        return ESR(self.n_landmarks, fern_cascades, self.mean_shape)
 
-            for i in xrange(n_samples):
-                normalized_offset = regressor.apply(pixels[i])
-                offset = util.transform_to_mean_shape(shapes[i], self.mean_shape).pseudoinverse().apply(normalized_offset).points
-                shapes[i].points += offset
-
+class ESR:
+    def __init__(self, n_landmarks, fern_cascades, mean_shape):
+        self.n_landmarks = n_landmarks
+        self.fern_cascades = fern_cascades
+        self.mean_shape = mean_shape
 
     def fit(self, image, initial_shape):
+        assert(initial_shape.n_points == self.n_landmarks)
         image = image.as_greyscale(mode='average')
-        shape = fit_shape_to_box(self.mean_shape, get_bounding_box(image))
+        shape = ESRBuilder.fit_shape_to_box(initial_shape, get_bounding_box(image))
 
-        for r in self.regressors:
-            mean_to_shape = util.transform_to_mean_shape(shape, self.mean_shape).pseudoinverse()
-            normalized_target = r.apply(r.extract_features(image, shape, mean_to_shape))
+        for r in self.fern_cascades:
+            mean_to_shape = transform_to_mean_shape(shape, self.mean_shape).pseudoinverse()
+            normalized_target = r.apply(image, shape, mean_to_shape)
             shape.points += mean_to_shape.apply(normalized_target).points
 
         return shape
--- util.py
+++ util.py
@@ -1,10 +1,9 @@
 import menpo
-import random
-import numpy as np
-import copy
 from menpo import io as mio
 from menpo.shape import PointCloud
+import numpy as np
 from menpo.transform import AlignmentSimilarity
+
 
 __author__ = 'andrejm'
 
@@ -15,39 +14,15 @@ def sample_image(im, points_to_sample):
             points_to_sample = points_to_sample.points
     return scipy_interpolation(im.pixels, points_to_sample, order=1,  mode='constant', cval=0).reshape((len(points_to_sample),))
 
-def translate_to_origin(src):
-    center = src.centre()
-    #src.points -= center
-    src = PointCloud([p - center for p in src.points])
-    return src
-
 def transform_to_mean_shape(src, mean_shape):
-    src = translate_to_origin(src)
+    centered = PointCloud(src.points - src.centre(), copy=False)
 
-    return AlignmentSimilarity(src, mean_shape)
+    return AlignmentSimilarity(centered, mean_shape)
 
-def getNormalisedMeanShape(img_path):
-    mean_shape = menpo.shape.mean_pointcloud([img.landmarks['PTS'].lms for img in mio.import_images(img_path)])
-    mean_shape = translate_to_origin(mean_shape)
-
-    l = zip(*list(mean_shape.points))
-    box = [ [min(l[0]), min(l[1])], [max(l[0]), max(l[1])] ]
-    w = box[1][0] - box[0][0]
-    h = box[1][1] - box[0][1]
-
-    return PointCloud([(p[0]/(w/2), p[1]/(h/2)) for p in mean_shape.points])
-
-
-def fit_shape_to_box(normal_shape, box):
-    w = box[1][0] - box[0][0]
-    h = box[1][1] - box[0][1]
-    box_center = [0, 0]
-    box_center[0] = (box[0][0] + box[1][0])/2.0
-    box_center[1] = (box[0][1] + box[1][1])/2.0
-
-    # TODO: Slow.
-    return PointCloud( [ (p[0]*w/2 + box_center[0], p[1]*h/2+box_center[1]) for p in normal_shape.points ])
-    # return PointCloud(normal_shape.points*np.array([w, h])/2 + box_center)
+def rand_unit_vector(dim):
+    ret = np.random.randn(dim)
+    ret /= np.linalg.norm(ret)
+    return ret
 
 # TODO: Use OpenCV for this!
 def get_bounding_box(img):
@@ -60,15 +35,4 @@ def get_bounding_box(img):
 
     return box
 
-def perturb(bounding_box):
-    width = bounding_box[1][0] - bounding_box[0][0]
-    height = bounding_box[1][1] - bounding_box[0][1]
 
-    ret = copy.deepcopy(bounding_box)
-
-    for i in xrange(2):
-        dx = random.uniform(-0.05*width, 0.05*width)
-        dy = random.uniform(-0.05*height, 0.05*height)
-        ret[i][0] += dx
-        ret[i][1] += dy
-    return ret
