f1b1e3da13c3a5a312d3e964a9b2462f9973a9f1
Andrej Maris <4ndrejm@gmail.com>
2015-05-23 15:41:10 +0100

    Basic refactoring

--- fern.py
+++ fern.py
@@ -3,6 +3,7 @@ from util import rand_unit_vector
 
 import cv2
 
+
 class FernBuilder:
     def __init__(self, n_pixels, n_features, n_landmarks, beta):
         self.n_pixels = n_pixels
@@ -11,32 +12,13 @@ class FernBuilder:
         self.beta = beta
 
     def _highest_correlated_pixel(self, dir, targets, cov_pp, pixel_values, pixel_averages, var_pp_sum):
-        # # Project each target onto random direction.
+        # Project each target onto random direction.
         lengths = targets.reshape((len(targets), 2*self.n_landmarks)).dot(dir)
         cov_l_p = pixel_values.dot(lengths)/len(targets) - np.average(lengths) * pixel_averages
         correlation = (cov_l_p[:, None] - cov_l_p) / np.sqrt(np.std(lengths) * (var_pp_sum - 2 * cov_pp))
 
-        # if np.isnan(correlation).all():
-        #     return 0, 0
         res = np.nanargmax(correlation)
         return res / self.n_pixels, res % self.n_pixels
-
-        # Random projection vector
-        #projection = np.random.normal(size=136)
-        # Project regression targets against random projection
-        #Y_proj = targets.reshape(len(targets), 136).dot(projection)
-        # Calculate covariance of projections
-        #Y_proj_cov = np.cov(Y_proj, bias=1)
-        # Calculate the pixel covariance
-        #Y_pixels_cov  = (((pixel_values * Y_proj).sum(axis=1) / len(targets)) -
-        #                 (Y_proj.sum() / len(targets)) * (pixel_values.sum(axis=1) / len(targets)))
-        # Calculate the indices of the largest covariance
-        #pixel_cov_diff = np.diag(cov_pp)[:, None] + np.diag(cov_pp)
-        #correlation = (Y_pixels_cov[:, None] - Y_pixels_cov) / np.sqrt(
-        #    Y_proj_cov * (pixel_cov_diff - 2 * cov_pp))
-        #res = np.nanargmax(correlation)
-
-        #return res/400, res%400
 
     @staticmethod
     def _get_features(pixel_samples, feature_indices):
@@ -50,8 +32,6 @@ class FernBuilder:
     def _calc_bin_averages(targets, bin_ids, n_features, n_landmarks, beta):
         bins = np.zeros((2 ** n_features, 2 * n_landmarks))
         bins_size = np.zeros((2 ** n_features,))
-        #bins[bin_ids] += targets.reshape(len(targets), 2 * n_landmarks)
-        #bins_size[bin_ids] += 1
         for i in xrange(len(bin_ids)):
             bins[bin_ids[i]] += targets[i].reshape(136,)
             bins_size[bin_ids] += 1
@@ -69,7 +49,6 @@ class FernBuilder:
 
         features = self._get_features(pixel_samples, feature_indices)
         ranges = features.min(axis=0), features.max(axis=0)
-        # thresholds = np.random.uniform(low=ranges[0], high=ranges[1])
         thresholds = (ranges[0]+ranges[1])/2.0 + np.random.uniform(low=-(ranges[1]-ranges[0])*0.1,
                                                                     high=(ranges[1]-ranges[0])*0.1)
 
@@ -109,16 +88,7 @@ class Fern:
         if not self.compressed:
             return self.bins[bin_id].reshape((self.n_landmarks, 2))
         else:
-            #return self._decompress_bin(bin_id, basis)
             return np.sum(basis[self.compressed_bins[bin_id]]*self.compressed_coeffs[bin_id].reshape(self.Q,1), axis=0)
-
-    # def _decompress_bin(self, bin_id, basis):
-    #     return basis[self.compressed_bins[bin_id]]*self.compressed_coeffs[bin_id]
-        # output = self.bins[bin_id]
-        # ret = np.zeros(self.n_landmarks*2)
-        # for (base_vector_id, coeff) in output:
-        #     ret += basis[int(base_vector_id)]*coeff
-        # return ret
 
     def compress(self, basis, Q):
         self.compressed = True
@@ -132,14 +102,6 @@ class Fern:
             # compressed_bin = np.zeros((Q, 2))
             residual = current_bin.copy()
             for k in xrange(Q):
-                # max_projection = 0.0
-                # max_i = 0
-                # for j, base_vector in enumerate(basis):
-                #     proj = residual.dot(base_vector)
-                #     if proj > max_projection:
-                #         max_projection = proj
-                #         max_i = j
-
                 max_i = np.argmax(basis.dot(residual))
 
                 self.compressed_bins[b][k] = max_i
@@ -152,11 +114,6 @@ class Fern:
                                        compressed_matrix_t.dot(current_bin), flags=cv2.DECOMP_SVD)
                 for j in xrange(k+1):
                     self.compressed_coeffs[b][j] = dst[j]
-                #dst = dst.reshape(2*self.n_landmarks, 1)
                 residual -= compressed_matrix.dot(dst).reshape(2*self.n_landmarks,)
-            #compressed_bins.append(compressed_bin)
-            # self.compressed_bins[i] = compressed_bin[:, 0]
-            # self.compressed_coeffs[i] = compressed_bin[:, 1]
 
         self.bins = None
-        #self.bins = compressed_bins
