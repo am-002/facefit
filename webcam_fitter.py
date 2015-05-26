24a1df171c8f5f3323b1af4f8f1a95f46a74afd4
Andrej Maris <4ndrejm@gmail.com>
2015-05-26 19:21:30 +0100

    Makes webcam_fitter get the path to the hikled model as a command line argument

--- webcam_fitter.py
+++ webcam_fitter.py
@@ -3,14 +3,14 @@ import menpo
 import numpy as np
 import hickle
 import menpodetect
-
+import sys
 def add_landmarks(mat, shape):
     for i in xrange(68):
         cv2.circle(mat, center=(int(shape.points[i][1]), int(shape.points[i][0])), radius=2, color=255, thickness=-1)
 
 
 cap = cv2.VideoCapture(0)
-model = hickle.load("blah.hkl", safe=False)
+model = hickle.load(sys.argv[1], safe=False)
 face_detector = menpodetect.load_dlib_frontal_face_detector()
 
 while True:
@@ -22,12 +22,11 @@ while True:
     img.pixels /= 255.0
 
     orig_shape = np.array(img.shape)
-    img = img.resize(orig_shape / 3.0)
+    img = img.resize(orig_shape)
 
     shapes = model.fit(img, face_detector(img))
 
     for shape in shapes:
-        shape.points *= 3
         add_landmarks(frame, shape)
 
     cv2.imshow('frame', frame)
